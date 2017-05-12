6b3e8c81ac6a46e7510acfbf35ecd6492bff9231
Charlie Jonas <charlie@charliejonas.co.uk>
2017-05-12 21:58:03 +0100

    Writing authentication backend

--- app/auth.php
+++ app/auth.php
@@ -1,37 +1,42 @@
-<?
+<?php
+
 require_once "php/common.php";
+
 if (array_key_exists("logout", $_GET)) {
     session_unset();
     $_SESSION["msg"] = array("info", "You're now logged out.");
 } else {
-    require_once "php/ucam_webauth.php";
-    $webauth = new Ucam_Webauth(array(
-        "hostname" => "rooms.rcsa.co.uk",
+    require_once "lib/ucam_webauth/ucam_webauth.php";
+    $authEngine = new Ucam_Webauth(array(
+        "hostname" => "roomballot.fitzjcr.com",
 	    "key_dir" => "/societies/rcsarooms/raven_keys",
-	    "cookie_key" => "VrcLskstLT562Tp8ebxLZNnM9tQMzbLy0ocBojchgoqhlQCA"
+	    "cookie_key" => "v225T29kPFaFjQCy754mrVVKVCXG8oCTkBZEAP9ohMz2sM5T5PYAGSGn3zVbZka7"
     ));
-    if (!$webauth->authenticate()) exit();
-    if ($webauth->success()) {
-        $_SESSION["user"] = $webauth->principal();
-        #        $group = posix_getgrnam("rcsarooms")
-        $group = array("members" => array("dec41", "bt332", "mt674", "cs845"));
-        $_SESSION["admin"] = in_array($_SESSION["user"], $group["members"]);
-        $curl = curl_init("https://www.lookup.cam.ac.uk/api/v1/person/crsid/" . $_SESSION["user"] . "/insts?format=json");
-        curl_setopt($curl, CURLOPT_USERPWD, "anonymous:");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $res = json_decode(curl_exec($curl), true);
-        curl_close($curl);
-        $_SESSION["member"] = false;
-        foreach ($res["result"]["institutions"] as $inst) {
-            if (in_array($inst["instid"], array("ROBIN", "ROBINUG", "ROBINPG")) && !$inst["cancelled"]) {
-                $_SESSION["member"] = true;
-                break;
-            }
-        }
+
+    // user does not authenticate sucessfully
+    if (!$authEngine->authenticate()) {
+        exit();
+    }
+
+    // user authenticates sucessfully
+    if ($authEngine->success()) {
+        // store CRSiD in the session token
+        $_SESSION["user"] = $authEngine->principal();
+
+        // check if user is a website administrator
+        // TODO read admin CRSiD's in from an external file that's easy to edit
+        $admins = array("members" => array("chtj2"));
+        $_SESSION["admin"] = in_array($_SESSION["user"], $admins["members"]);
+
+        // check if the user is a member of Fitzwilliam College
+        // TODO read member CRSiD's in from an external file that's easy to edit
+        $members = array("members" => array("chtj2"));
+        $_SESSION["member"] = in_array($_SESSION["user"], $members["members"]);
+
         if ($_SESSION["member"]) {
-            $_SESSION["msg"] = array("success", "You're now logged in!");
+            $_SESSION["msg"] = array("success", "Logged in sucessfully. Authenticated as a Fitzwilliam College JCR Member.");
         } else {
-            $_SESSION["msg"] = array("warning", "You're logged in, but you don't appear to be a Robinson College member.  You can still browse the site if you want though...");
+            $_SESSION["msg"] = array("warning", "Logged in sucessfully. Failed to authenticated as a Fitzwilliam College JCR Member, but you can still browse some areas of the site.");
         }
     } else {
         $_SESSION["msg"] = array("danger", "Failed to login (" . $webauth->status() . "): " . $webauth->msg());
