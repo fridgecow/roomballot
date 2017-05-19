705402fa2cd9477a85dd2e40b25cf67f2bc5f843
Charlie Jonas <charlie@charliejonas.co.uk>
2017-05-19 23:56:04 +0100

    Correcting indentation

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ Layout::HTMLheader("Fitz JCR Housing Ballot System");
 Layout::navbar();
 ?>
 
-            <h1>Fitz JCR Housing Ballot System</h1>
-            <p>For security reasons, we request that you keep information regarding room rents and photographs of College-owned rooms strictly confidential to the members of Fitzwilliam College. If anyone is known to have violated this rule, all information will be withdrawn and the person in question will be referred to the Senior Tutor and the Dean.</p>
-            <p>If you have any problem with the housing ballot you should contact the JCR Vice President at <a href="mailto:jcr.vice.president@fitz.cam.ac.uk">jcr.vice.president@fitz.cam.ac.uk</a>. To report any technical issues with this website you are invited to contact the JCR Website Officer in confidence at <a href="mailto:jcr.website@fitz.cam.ac.uk">jcr.website@fitz.cam.ac.uk</a>.</p>
+        <h1>Fitz JCR Housing Ballot System</h1>
+        <p>For security reasons, we request that you keep information regarding room rents and photographs of College-owned rooms strictly confidential to the members of Fitzwilliam College. If anyone is known to have violated this rule, all information will be withdrawn and the person in question will be referred to the Senior Tutor and the Dean.</p>
+        <p>If you have any problem with the housing ballot you should contact the JCR Vice President at <a href="mailto:jcr.vice.president@fitz.cam.ac.uk">jcr.vice.president@fitz.cam.ac.uk</a>. To report any technical issues with this website you are invited to contact the JCR Website Officer in confidence at <a href="mailto:jcr.website@fitz.cam.ac.uk">jcr.website@fitz.cam.ac.uk</a>.</p>
 
 <?php
 Layout::HTMLfooter();
