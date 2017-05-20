5839bd21c789dd8b5de36b64327babd2e7b27fed
Charlie Jonas <charlie@charliejonas.co.uk>
2017-05-20 22:47:34 +0100

    Fix for php 7.0

--- app/Environment.php
+++ app/Environment.php
@@ -3,17 +3,17 @@
 class Environment {
 
     // app enviromental variables
-    public const $app_hostname = "chtj2.user.srcf.net"
+    const app_hostname = "chtj2.user.srcf.net";
 
     // database enviromental variables
-    public const $db_host = "localhost";     // database host machine
-    public const $db_user = "";              // database username
-    public const $db_pass = "";              // database password
-    public const $db_name = "";              // database name
-    public const $db_prefix = "";            // table prefix
+    const db_host = "localhost";     // database host machine
+    const db_user = "";              // database username
+    const db_pass = "";              // database password
+    const db_name = "";              // database name
+    const db_prefix = "";            // table prefix
 
     // Raven authentication module enviromental variables
-    public const $raven_cookieKey = "v225T29kPFaFjQCy754mrVVKVCXG8oCTkBZEAP9ohMz2sM5T5PYAGSGn3zVbZka7";       // this needs to be 64+ random characters
+    const raven_cookieKey = "v225T29kPFaFjQCy754mrVVKVCXG8oCTkBZEAP9ohMz2sM5T5PYAGSGn3zVbZka7";       // this needs to be 64+ random characters
 
 }
 
