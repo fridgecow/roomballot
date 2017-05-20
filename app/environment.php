a23a563cf6c5484a7a3a8a470aea9cc50d031d4d
Charlie Jonas <charlie@charliejonas.co.uk>
2017-05-20 22:14:16 +0100

    env vars aren't really variables but are constants

--- app/environment.php
+++ app/environment.php
@@ -3,17 +3,17 @@
 class Environment {
 
     // app enviromental variables
-    public static $app_hostname = "chtj2.user.srcf.net"
+    public const $app_hostname = "chtj2.user.srcf.net"
 
     // database enviromental variables
-    public static $db_host = "localhost";     // database host machine
-    public static $db_user = "";              // database username
-    public static $db_pass = "";              // database password
-    public static $db_name = "";              // database name
-    public static $db_prefix = "";            // table prefix
+    public const $db_host = "localhost";     // database host machine
+    public const $db_user = "";              // database username
+    public const $db_pass = "";              // database password
+    public const $db_name = "";              // database name
+    public const $db_prefix = "";            // table prefix
 
     // Raven authentication module enviromental variables
-    public static $raven_cookieKey = "v225T29kPFaFjQCy754mrVVKVCXG8oCTkBZEAP9ohMz2sM5T5PYAGSGn3zVbZka7";       // this needs to be 64+ random characters
+    public const $raven_cookieKey = "v225T29kPFaFjQCy754mrVVKVCXG8oCTkBZEAP9ohMz2sM5T5PYAGSGn3zVbZka7";       // this needs to be 64+ random characters
 
 }
 
