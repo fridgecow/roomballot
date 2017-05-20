18435fdfa0f65a9570491baa913880b151f36d70
Charlie Jonas <charlie@charliejonas.co.uk>
2017-05-20 22:11:30 +0100

    Would help if the env vars were readable by others

--- app/environment.php
+++ app/environment.php
@@ -3,17 +3,17 @@
 class Environment {
 
     // app enviromental variables
-    private static $app_hostname = "chtj2.user.srcf.net"
+    public static $app_hostname = "chtj2.user.srcf.net"
 
     // database enviromental variables
-    private static $db_host = "localhost";     // database host machine
-    private static $db_user = "";              // database username
-    private static $db_pass = "";              // database password
-    private static $db_name = "";              // database name
-    private static $db_prefix = "";            // table prefix
+    public static $db_host = "localhost";     // database host machine
+    public static $db_user = "";              // database username
+    public static $db_pass = "";              // database password
+    public static $db_name = "";              // database name
+    public static $db_prefix = "";            // table prefix
 
     // Raven authentication module enviromental variables
-    private static $raven_cookieKey = "v225T29kPFaFjQCy754mrVVKVCXG8oCTkBZEAP9ohMz2sM5T5PYAGSGn3zVbZka7";       // this needs to be 64+ random characters
+    public static $raven_cookieKey = "v225T29kPFaFjQCy754mrVVKVCXG8oCTkBZEAP9ohMz2sM5T5PYAGSGn3zVbZka7";       // this needs to be 64+ random characters
 
 }
 
