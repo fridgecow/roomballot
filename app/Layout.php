e305112accaaa86ea038f6ffffe7005d3ff7d2a6
Charlie Jonas <charlie@charliejonas.co.uk>
2017-05-21 15:42:02 +0100

    Add favicon

--- app/Layout.php
+++ app/Layout.php
@@ -17,7 +17,7 @@ class Layout {
 		<meta name="title" content="<?php echo $pageTitle; ?>">
 		<meta name="description" content="">
 		<title><?php echo $pageTitle; ?></title>
-		<!--<link href="include/img/icon.ico" rel="shortcut icon">-->
+		<link href="include/img/icon.ico" rel="shortcut icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <link rel="stylesheet" href="include/css/sticky-footer-navbar.css">
