503d32b6aecc5c40119a7aa203e9b736d3d9377e
Charlie Jonas <charlie@charliejonas.co.uk>
2017-05-21 16:36:55 +0100

    Keep track of News columns

--- app/News.php
+++ app/News.php
@@ -2,6 +2,9 @@
 
 class News {
 
+    // number of news items in a row (max 3)
+    private static $rowCount = 0;
+
     public static function HTMLtop() {
 ?>
 
@@ -11,12 +14,20 @@ class News {
     }
 
     public static function HTMLrow($heading, $content) {
+        if (self::$rowCount == 3) {
+?>
+            </div>
+            <div class="row">
+<?php
+            self::$rowCount = 0;
+        }
 ?>
                 <div class="col-md-4">
                     <h2><?php echo $heading; ?></h2>
                     <?php echo $content; ?>
                 </div>
 <?php
+        self::$rowCount += 1;
     }
 
     public static function HTMLbottom() {
