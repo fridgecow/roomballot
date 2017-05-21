4f3d37586be0ab694b36e06f796ae964f2f47b99
Charlie Jonas <charlie@charliejonas.co.uk>
2017-05-21 16:04:26 +0100

    Login credentials in the repo!

--- app/Environment.php
+++ app/Environment.php
@@ -3,17 +3,17 @@
 class Environment {
 
     // app enviromental variables
-    const app_hostname = "chtj2.user.srcf.net";
+    const app_hostname = "";
 
     // database enviromental variables
     const db_host = "localhost";     // database host machine
-    const db_user = "chtj2";              // database username
-    const db_pass = "VMkUFakJihsoXa8F22XVwBMUNvXvsmn5eCAQ5MngcmhCZX7frBZj8YgZAyBeW83g";              // database password
-    const db_name = "fitzjcr/housingballotsystem";              // database name
+    const db_user = "";              // database username
+    const db_pass = "";              // database password
+    const db_name = "";              // database name
     const db_prefix = "";            // table prefix
 
     // Raven authentication module enviromental variables
-    const raven_cookieKey = "v225T29kPFaFjQCy754mrVVKVCXG8oCTkBZEAP9ohMz2sM5T5PYAGSGn3zVbZka7";       // this needs to be 64+ random characters
+    const raven_cookieKey = "";       // this needs to be 32+ random characters
 
 }
 
