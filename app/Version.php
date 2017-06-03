fc5ad36de5ebbb6f577756ea0d51a67821e759c0
Charlie Jonas <charlie@charliejonas.co.uk>
2017-06-03 14:48:48 +0100

    Use commit number and bump

--- app/Version.php
+++ app/Version.php
@@ -2,14 +2,14 @@
 
 class Version {
     const MAJOR = 0;
-    const MINOR = 1;
-    const PATCH = 107;
+    const MINOR = 2;
+    const PATCH = 0;
 
     public static function getVersion() {
         $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
         $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
         $commitDate->setTimezone(new \DateTimeZone('UTC'));
-        return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:m:s'));
+        return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, $commitNumber , $commitHash, $commitDate->format('Y-m-d H:m:s'));
     }
 }
 
